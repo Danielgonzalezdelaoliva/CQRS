c25fffb9c4c25dfdd5bb2fa7aae59cf1dc97e1cf
Daniel Gonzalez De la Oliva <dgonzalezdelaoliva@mecalux.com>
2023-02-10 17:48:47 +0100

    CQRS

---
+++ MediatrValidationExample/src/MediatrValidationExample/Features/Products/Commands/CreateProductCommand.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using MediatrValidationExample.Domain;
+using MediatrValidationExample.Infrastructure.Persistence;
+
+namespace MediatrValidationExample.Features.Products.Commands;
+
+public class CreateProductCommand : IRequest
+{
+    public string Description { get; set; } = default!;
+    public double Price { get; set; }
+}
+
+
+public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand>
+{
+    private readonly MyAppDbContext _context;
+
+    public CreateProductCommandHandler(MyAppDbContext context)
+    {
+        _context = context;
+    }
+
+
+    public async Task<Unit> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        var newProduct = new Product
+        {
+            Description = request.Description,
+            Price = request.Price
+        };
+
+        _context.Products.Add(newProduct);
+
+        await _context.SaveChangesAsync();
+
+        return Unit.Value;
+    }
+}
